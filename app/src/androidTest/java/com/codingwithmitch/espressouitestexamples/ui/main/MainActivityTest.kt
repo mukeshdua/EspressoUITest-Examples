f43a13f09ba379332693709850306cf37e2f30b7
Mukesh Dua <mukesh.dua@noom.com>
2022-11-05 20:39:40 -0700

    Modified to learn

--- app/src/androidTest/java/com/codingwithmitch/espressouitestexamples/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/codingwithmitch/espressouitestexamples/ui/main/MainActivityTest.kt
@@ -1,11 +1,13 @@
 package com.codingwithmitch.espressouitestexamples.ui.main
 
-import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.codingwithmitch.espressouitestexamples.R
+import org.junit.Rule
 import org.junit.Test
 
 import org.junit.runner.RunWith
@@ -13,36 +15,18 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4ClassRunner::class)
 class MainActivityTest{
 
+    @get:Rule
+    val activityScenario:ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
     fun testActivity_inView() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-
+        onView(withId(R.id.main)).check(matches(isDisplayed()))
+        onView(withId(R.id.button_next_activity)).perform(click())
+        onView(withId(R.id.button_back)).perform(click())
         onView(withId(R.id.main)).check(matches(isDisplayed()))
     }
 
-    // Visibility
-    @Test
-    fun testVisibility_title_nextButton() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
 
-        onView(withId(R.id.activity_main_title))
-            .check(matches(isDisplayed())) // method 1
-
-        onView(withId(R.id.activity_main_title))
-            .check(matches(withEffectiveVisibility(Visibility.VISIBLE))) // method 2
-
-        onView(withId(R.id.button_next_activity))
-            .check(matches(isDisplayed()))
-    }
-
-    // Text
-    @Test
-    fun testTitleTextDisplayed() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-
-        onView(withId(R.id.activity_main_title))
-            .check(matches(withText(R.string.text_mainactivity)))
-    }
 
 }
 
